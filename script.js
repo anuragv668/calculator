895cae47772621a6f68c821b1dca5d255a3a1252
anuragv668 <anuragvarma668@gmail.com>
2023-11-16 16:24:06 +0530

    create operate function

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   return a / b;
 };
+
+let num1;
+let num2;
+let operator;
+
+const operate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+      break;
+    case '-':
+      return subtract(num1, num2);
+      break;
+    case '*':
+      return multiply(num1, num2);
+      break;
+    case '/':
+      return divide(num1, num2);
+      break;
+  }
+};
